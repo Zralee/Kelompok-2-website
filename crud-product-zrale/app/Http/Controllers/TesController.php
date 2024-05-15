c5d3cbd44bccc23ef6dfde17d88b995a8d1cc6e6
Zralee <guspramana03@gmail.com>
2024-05-15 09:42:17 +0800

    Update TesController.php

--- crud-product-zrale/app/Http/Controllers/TesController.php
+++ crud-product-zrale/app/Http/Controllers/TesController.php
@@ -6,5 +6,12 @@ use Illuminate\Http\Request;
 
 class TesController extends Controller
 {
-    //
+     public function index() : View
+    {
+        //get all products
+        $products = Product::latest()->paginate(10);
+
+        //render view with products
+        return view('products.index', compact('products'));
+    }
 }
